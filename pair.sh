737926dd33fe2412fbd617a9c9bc08bf392b9016
R0G3_M <45758962+mohamed-chemlali@users.noreply.github.com>
2021-06-07 19:12:11 +0100

    Update pair.sh

--- pair.sh
+++ pair.sh
@@ -4,5 +4,4 @@
 ip =`curl -X GET https://api.ipify.org`
 
 # pair the scan engine with console
-curl -X POST -H "Content-Type: application/json" -d '{"address": $ip,"id": 6,"name": "Corporate Scan Engine 001","port": 40814}
-
+curl -k -X POST -H "Content-Type: application/json" -d '{"address": $ip,"id": 6,"name": "Corporate Scan Engine 001","port": 40814}' https://console-ip:3780/api/3/scan_engines
